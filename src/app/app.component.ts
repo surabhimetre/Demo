0891a73ef0e062c58f0d09cee7dc215f6d12d728
Surabhi <surabhimetre@gmail.com>
2019-03-21 09:41:15 +0530

    'First'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,47 @@
-import { Component } from '@angular/core';
+/** import { Component } from '@angular/core';
 
 @Component({
   selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+  templateUrl:'app/app.component.html'
 })
-export class AppComponent  { name = 'Angular'; }
+export class AppComponent  { 
+  appTitle: string ='Welcome';
+  /** appStatus: boolean = true;*/
+/**  appList: any[]= [ {
+    "ID" : "1",
+    "Name" : "One"
+  },
+  {
+    "ID" : "2",
+    "Name" : "Two"
+  } ];
+  appImage: any[] = [ {
+    "ID" : "1",
+    "url": 'app/images/one.jpg'
+  }];
+ }
+*/
+import { Component } from '@angular/core';
+import { IProduct } from './product';
+import { ProductService } from './product.service';
+import { appService } from './app.service';
+import { Http , Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+
+@Component ({
+   selector: 'my-app',
+   template: '<div>Hello</div>',
+   providers: [ProductService]
+})
+
+export   class   AppComponent  {
+   iproducts: IProduct[];
+   constructor(private _product: ProductService) {
+   }
+   
+   ngOnInit() : void {
+      this._product.getproducts()
+      .subscribe(iproducts => this.iproducts = iproducts);
+   }
+}
